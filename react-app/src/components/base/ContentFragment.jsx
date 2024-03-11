9efdf6ff3e7d64400ab58c80de7ebd5d1d295a70
Gilles Knobloch <gknob@adobe.com>
2024-03-11 22:58:57 +0100

    Cleanup

--- react-app/src/components/base/ContentFragment.jsx
+++ react-app/src/components/base/ContentFragment.jsx
@@ -4,19 +4,7 @@ const ContentFragment = ({
   children,
   tag,
   className,
-  cf,
-  label,
 }) => {
-  let title = "";
-  if (cf?._metadata?.stringMetadata) {
-    title =
-      cf?._metadata?.stringMetadata.filter((meta) => meta?.name === "title")[0]
-        ?.value || null;
-  }
-
-  const compositeLabel =
-    label || cf?._model?.title + (title ? ` (${title})` : "");
-
   const Component = tag || "div";
 
   return (
