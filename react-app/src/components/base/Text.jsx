a844b3123fc1f6def596e7064b7a3d0dc8aabce6
Gilles Knobloch <gknob@adobe.com>
2024-03-11 16:18:53 +0100

    In-context editing of text properties

--- react-app/src/components/base/Text.jsx
+++ react-app/src/components/base/Text.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
 const Text = ({ children, content, className, prop, label, behavior }) => {
+  const editorProps = {
+    "data-aue-prop": prop,
+  };
+
   let Component = null;
 
   if (children || content?.plaintext) {
     Component = (
-      <div className={className}>
+      <div {...editorProps} data-aue-type="text" className={className}>
         {children || content.plaintext}
       </div>
     );
   } else if (content?.html) {
     Component = (
       <div
+        {...editorProps}
+        data-aue-type="richtext"
         className={className}
         dangerouslySetInnerHTML={{ __html: content.html }}
       />
--- react-app/src/components/base/Title.jsx
+++ react-app/src/components/base/Title.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Title = ({ children, heading, className, prop, label, behavior }) => {
+  const editorProps = {
+    "data-aue-prop": prop,
+    "data-aue-type": "text",
+  };
+
   const Component = heading || "h1";
 
   return (
-    <Component className={className}>
+    <Component className={className} {...editorProps}>
       {children || ""}
     </Component>
   );
