d5fe58174fab24a29b12ed2d2cb982f37d9c6acb
Gilles Knobloch <gknob@adobe.com>
2024-03-11 19:17:23 +0100

    Image

--- react-app/src/components/base/Image.jsx
+++ react-app/src/components/base/Image.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import { getURI } from "../../utils";
 
 const Image = ({ src, alt, className, prop, type, label, behavior }) => {
+  const editorProps = {
+    "data-aue-prop": prop || "",
+    "data-aue-type": type || "media",
+    "data-aue-label":
+      label || (prop && prop[0].toUpperCase() + prop.slice(1)) || "",
+  };
+
   return (
     <img
       src={getURI(src || "")}
       alt={alt}
       className={className}
+      {...editorProps}
     />
   );
 };
