9959d00c5fae359aed70020717f11dd9d19d7206
Gilles Knobloch <gknob@adobe.com>
2024-03-11 20:21:06 +0100

    Container

--- react-app/src/components/base/Container.jsx
+++ react-app/src/components/base/Container.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const Container = ({ children, tag, className, prop, label, filter }) => {
+  const editorProps = {
+    "data-aue-type": "container",
+    "data-aue-prop": prop,
+    "data-aue-label": label,
+  };
+
   const Component = tag || "div";
 
   return (
-    <Component className={className}>
+    <Component className={className} {...editorProps}>
       {children}
     </Component>
   );
