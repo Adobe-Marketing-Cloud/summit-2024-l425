8d90bbf78cbe5d484bad3edeacdb19788c7566c2
voss <voss@adobe.com>
2025-05-29 16:55:48 +0200

    Make service flexible

--- react-app/web-src/src/App.jsx
+++ react-app/web-src/src/App.jsx
@@ -10,6 +10,7 @@ import ArticleDetail from "./pages/ArticleDetail.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import { getURI } from "./utils";
 import "./App.scss";
+import {getServiceURL} from "./utils/getURI";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
             name="urn:adobe:aue:system:aemconnection"
             content={`aem:${getURI()}`}
           />
+          { getServiceURL() && <meta name="urn:adobe:aue:config:service" content={getServiceURL()}/> }
           <script type="application/vnd.adobe.aue.component+json" src="./static/component-definition.json"></script>
           <script type="application/vnd.adobe.aue.model+json" src="./static/filter-definition.json"></script>
         </Helmet>
--- react-app/web-src/src/utils/getURI.js
+++ react-app/web-src/src/utils/getURI.js
@@ -5,3 +5,12 @@ const serviceURL = REACT_APP_USE_PROXY === "true" ? "/" : REACT_APP_HOST_URI;
 export const getURI = (path = "") => {
   return serviceURL + path;
 };
+
+export const getServiceURL = () => {
+  const url = new URL(window.location.href);
+  const searchParams = new URLSearchParams(url.search);
+  if (searchParams.has("service")) {
+    return searchParams.get("service");
+  }
+  return null;
+};
